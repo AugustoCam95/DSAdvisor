323197513e22eb8a8207eaf55821179d9b528ce0
José Augusto Câmara Filho <augustocam95@gmail.com>
2021-05-25 23:14:01 -0300

    pagination fix

--- src/delete.py
+++ src/delete.py
@@ -10,7 +10,7 @@ def delete_trash():
     #     os.remove("output.zip")
 
     #Remover arquivos csv's
-    os.chdir(start_point+"/static/samples")
+    os.chdir(start_point+"/src/static/samples")
     folder1 = os.listdir()
     for i in range(len(folder1)):
         if ".csv" in folder1[i]:
@@ -18,7 +18,7 @@ def delete_trash():
     os.chdir(start_point)
 
     #Remove csv's da dataset
-    os.chdir(start_point+"/static/uploads/dataset")
+    os.chdir(start_point+"/src/static/uploads/dataset")
     folder2 = os.listdir()
     for i in range(len(folder2)):
         if ".csv" in folder2[i]:
