3b73ce5d578e587b008df5d9fdfe932207659b4d
José Augusto <augustocam95@gmail.com>
2021-09-21 08:25:13 -0300

    Update application.py
    Remove debug mode

--- application.py
+++ application.py
@@ -421,5 +421,5 @@ def return_files_test():
 
 if __name__ == "__main__":    
     port = int(os.environ.get("PORT", 5000))
-    application.run(host='0.0.0.0', port=port, debug= True)
+    application.run(host='0.0.0.0', port=port, debug= False)
     
